b6d2d83604c657321963f437e992d8e233bfb04a
Mr Speaker <mrspeaker@gmail.com>
2019-06-03 13:45:41 -0400

    REmove Fira default

--- init.el
+++ init.el
@@ -167,7 +167,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fira Code" :foundry "CTDB" :slant normal :weight normal :height 98 :width normal))))
  '(sunshine-forecast-headline-face ((t (:foreground "navajo white" :height 1.1)))))
 
 ; Line-wrap icons on right side only.
