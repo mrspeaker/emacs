00f99b96a62cb7af4e33e773847ec8bd31634b00
Mr Speaker <mrspeaker@gmail.com>
2019-06-03 13:54:04 -0400

    Add Liberation font

--- init.el
+++ init.el
@@ -167,6 +167,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Liberation Mono" :foundry "1ASC" :slant normal :weight normal :height 113 :width normal))))
  '(sunshine-forecast-headline-face ((t (:foreground "navajo white" :height 1.1)))))
 
 ; Line-wrap icons on right side only.
