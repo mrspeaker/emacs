c12c7497a48d41511f2af023719b2553d2589f93
Mr Speaker <mrspeaker@gmail.com>
2019-06-17 11:40:51 -0400

    Start moving init.el to config.org

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
 (setq-default js2-tab-width 2)
 (setq-default tab-width 2)
 (setq-default js2-basic-offset 2)
-(setq js-indent-level 2) 
+(setq js-indent-level 2)
 
 ; Font
 (setq-default line-spacing 0.12)
@@ -144,43 +144,9 @@
 (setq erc-lurker-hide-list '("JOIN" "PART" "QUIT"))
 (setq erc-lurker-threshold-time 3600)
 
-; Org mode
-(setq org-todo-keywords
-      '((sequence "TODO" "WIP" "DONE")))
-
-;; Don't smash frames
-(setq org-agenda-window-setup 'current-window)
-
-;; Babel exec code
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '(
-   (js . t)
-   ))
-(setq org-src-fontify-natively t) ; syntax highlight
-(setq org-confirm-babel-evaluate nil) ; don't ask to exec
-(setq org-src-tab-acts-natively t) ; tab in code blocks
-
-; Org-capture
-(global-set-key (kbd "C-c c") 'org-capture)
-(setq org-default-notes-file "~/notes/organizer.org")
-
-(setq org-capture-templates
-      '(("t" "Personal Task" entry
-         (file+headline org-default-notes-file "Tasks")
-         "* TODO %? %u")
-        ("w" "Work-related Task" entry
-         (file+headline "~/notes/mt/work.org" "New")
-         "* TODO %? %u")
-        ("r" "Read later" entry
-         (file+headline org-default-notes-file "Read Later")
-         "* TODO %?\n%u - %c")
-        ("g" "game idea" entry
-         (file "~/notes/gameideas.org")
-         "* %? %u")
-        ("i" "random idea" entry
-         (file "~/notes/ideas.org")
-         "* %? %u")))
+; TODO: In the process of moving init.el to an org file
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -193,34 +159,61 @@
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(auto-dim-other-buffers-mode t)
+ '(beacon-color "#cc6666")
  '(company-backends
    (quote
     (company-tern company-tide company-bbdb company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
                   (company-dabbrev-code company-gtags company-etags company-keywords)
                   company-oddmuse company-dabbrev)))
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(css-indent-offset 2)
+ '(custom-enabled-themes (quote (gruvbox-light-hard)))
  '(custom-safe-themes
    (quote
-    ("bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "e68cf26c0e2957f19cd8e857d9546b17a32ded41caa00a4f94c8276b2c19cb2b" "217ad4914762a1ff7dfb7dc55df85e37c11755fb2bb0c8a5df272e425bf669d9" "f87f74ecd2ff6dc433fb4af4e76d19342ea4c50e4cd6c265b712083609c9b567" default)))
+    ("a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" "2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" "b583823b9ee1573074e7cbfd63623fe844030d911e9279a7c8a5d16de7df0ed0" "8e797edd9fa9afec181efbfeeebf96aeafbd11b69c4c85fa229bb5b9f7f7e66c" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "e68cf26c0e2957f19cd8e857d9546b17a32ded41caa00a4f94c8276b2c19cb2b" "217ad4914762a1ff7dfb7dc55df85e37c11755fb2bb0c8a5df272e425bf669d9" "f87f74ecd2ff6dc433fb4af4e76d19342ea4c50e4cd6c265b712083609c9b567" default)))
  '(desktop-save (quote ask-if-new))
+ '(fci-rule-color "#373b41")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(flycheck-typescript-tslint-config "tsconfig.json")
  '(flycheck-typescript-tslint-executable "/home/mrspeaker/.nvm/versions/node/v11.1.0/bin/tslint")
+ '(frame-background-mode (quote dark))
  '(hl-paren-colors
    (quote
     ("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11")))
- '(md4rd-subs-active (quote (emacs gamedev spacex orgmode fortnitebr)) t)
+ '(md4rd-subs-active (quote (emacs gamedev spacex orgmode fortnitebr)))
  '(neo-show-slash-for-folder nil)
  '(org-agenda-files (quote ("~/work.org")))
  '(org-startup-indented t)
  '(org-startup-with-inline-images t)
  '(package-selected-packages
    (quote
-    (company-tern tern company tide md4rd auto-dim-other-buffers all-the-icons-dired all-the-icons markdown-mode flycheck slack typescript-mode magit yasnippet-snippets yasnippet expand-region hackernews org-download url-http-ntlm js2-mode)))
+    (php-mode gruvbox-theme company-tern tern company tide md4rd auto-dim-other-buffers all-the-icons-dired all-the-icons markdown-mode flycheck slack typescript-mode magit yasnippet-snippets yasnippet expand-region hackernews org-download url-http-ntlm js2-mode)))
  '(sunshine-appid "4a4924c8eb9b826d500afed6ea276dce")
  '(sunshine-location "11217,US")
  '(sunshine-show-icons t)
  '(sunshine-units (quote metric))
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#cc6666")
+     (40 . "#de935f")
+     (60 . "#f0c674")
+     (80 . "#b5bd68")
+     (100 . "#8abeb7")
+     (120 . "#81a2be")
+     (140 . "#b294bb")
+     (160 . "#cc6666")
+     (180 . "#de935f")
+     (200 . "#f0c674")
+     (220 . "#b5bd68")
+     (240 . "#8abeb7")
+     (260 . "#81a2be")
+     (280 . "#b294bb")
+     (300 . "#cc6666")
+     (320 . "#de935f")
+     (340 . "#f0c674")
+     (360 . "#b5bd68"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -229,71 +222,3 @@
  '(default ((t (:family "Liberation Mono" :foundry "1ASC" :slant normal :weight normal :height 120 :width normal))))
  '(auto-dim-other-buffers-face ((t (:background "#111"))))
  '(sunshine-forecast-headline-face ((t (:foreground "navajo white" :height 1.1)))))
-
-; Line-wrap icons on right side only.
-(setf (cdr (assq 'continuation fringe-indicator-alist))
-			'(nil right-curly-arrow)
-			)
-
-; Menus and scrollbars
-;(menu-bar-mode 1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-(desktop-save-mode 1) ; Save layout/open files
-;; note: this didn't save layout until I customised: '(desktop-save (quote ask-if-new))
-
-(delete-selection-mode 1) ; Overwrite current text selection
-
-(global-set-key (kbd "<mouse-3>") 'mouse-major-mode-menu)
-(global-set-key (kbd "<C-mouse-3>") 'mouse-popup-menubar)
-
-(require 'expand-region)
-(global-set-key (kbd "C-'") 'er/expand-region)
-
-(put 'narrow-to-region 'disabled nil) ; TODO: why?
-(put 'dired-find-alternate-file 'disabled nil) ; TODO: why?
-
-;; Move lines up/down with M-p and M-n
-(defun move-line-up ()
-  "Move up the current line."
-  (interactive)
-  (transpose-lines 1)
-  (forward-line -2)
-  (indent-according-to-mode))
-
-(defun move-line-down ()
-  "Move down the current line."
-  (interactive)
-  (forward-line 1)
-  (transpose-lines 1)
-  (forward-line -1)
-  (indent-according-to-mode))
-
-(global-set-key (kbd "M-p")  'move-line-up)
-(global-set-key (kbd "M-n")  'move-line-down)
-
-; Reddit Mode
-(setq md4rd-subs-active '(gamedev emacs orgmode spacex))
-
-;(set-background-color "#1f1915")
-
-; quiet, please! No dinging!
-(setq visible-bell nil
-      ring-bell-function 'flash-mode-line)
-(defun flash-mode-line ()
-  ;(invert-face 'mode-line)
-  ;(run-with-timer 0.1 nil #'invert-face 'mode-line))
-  (let ((orig-fg (face-foreground 'mode-line)))
-          (set-face-foreground 'mode-line "#F2804F")
-          (run-with-idle-timer 0.1 nil
-                               (lambda (fg) (set-face-foreground 'mode-line fg))
-                               orig-fg)))
-
-; Repeatedly use C-spc after C-u spc to cycle through mark ring
-(setq set-mark-command-repeat-pop t)
-(global-set-key [remap upcase-region] 'ignore)  ; because it triggers whenever i typo c-x u
-(global-set-key [remap org-cycle-agenda-files] 'ignore) ; tmp - same keybind as expand rgion above
-; (global-set-key [remap flyspell-autocorrect-previous-word] 'ignore)
-
-;;; ; tmp - same keybind as expand rgion above
