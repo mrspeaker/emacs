c6c780165408a9caf017b75af3ae6044a13b5a9a
Mr Speaker <mrspeaker@gmail.com>
2019-08-06 12:13:42 -0400

    Clean up init, add test eww setting

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+;;; init.el --- Emacs init file
+;;  Author: Mr Speaker
+;;; Commentary:
+;;  My very own Emacs config file
+;;; Code:
 (require 'package)
 (setq package-enable-at-startup nil)
 
@@ -8,9 +13,11 @@
 
 (package-initialize)
 
-
 (setenv "PATH" (concat "/usr/local/bin:"
-                (getenv "PATH")))
+                       (getenv "PATH")))
+
+(setq shr-color-visible-luminance-min 70) ; nicer eww background
+(global-auto-revert-mode) ;; testing this
 
 ;;; Bootstrapping use-package
 (unless (package-installed-p 'use-package)
@@ -24,4 +31,6 @@
 ; Custom vars not stored in config.
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
-       (load custom-file :noerror))
+  (load custom-file :noerror))
+
+;;; init.el ends here
